3dbbaa2905657a59f4d29237cbcfc39d03fcb013
kaylorchen <kaylor.chen@qq.com>
2024-01-22 09:58:40 +0800

    Update cmake

--- toolchain-aarch64.cmake
+++ toolchain-aarch64.cmake
@@ -8,10 +8,10 @@ set(CMAKE_SYSTEM_PROCESSOR aarch64)
 set(CMAKE_C_COMPILER /usr/bin/aarch64-linux-gnu-gcc)
 set(CMAKE_CXX_COMPILER /usr/bin/aarch64-linux-gnu-g++)
 
-set(CMAKE_SYSROOT /opt/data/orangepi/rk3588_dev_rootfs/rootfs)
+set(CMAKE_SYSROOT /opt/kaylor/data/work/rockchip/rk3588_dev_rootfs/rootfs)
 
 # 指定目标环境的根目录（sysroot）
-set(CMAKE_FIND_ROOT_PATH /opt/data/orangepi/rk3588_dev_rootfs/rootfs)
+set(CMAKE_FIND_ROOT_PATH /opt/kaylor/data/work/rockchip/rk3588_dev_rootfs/rootfs)
 
 # 设置是否使用目标环境中的程序
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
@@ -22,19 +22,3 @@ set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 # 设置在查找头文件时是否只查找目标环境中的头文件
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
-# 通知CMake要使用自定义的pkg-config文件
-set(ENV{PKG_CONFIG_DIR} "${CMAKE_SYSROOT}/usr/lib/pkgconfig")
-set(ENV{PKG_CONFIG_LIBDIR} "${CMAKE_SYSROOT}/usr/lib/pkgconfig:${CMAKE_SYSROOT}/usr/share/pkgconfig")
-set(ENV{PKG_CONFIG_SYSROOT_DIR} ${CMAKE_SYSROOT})
-
-# set(CMAKE_STAGING_PREFIX ${CMAKE_SYSROOT}/usr)
-# # 当进行交叉编译时，指定头文件的搜索路径
-# include_directories(${CMAKE_STAGING_PREFIX}/include)
-
-# # 添加需要链接的库文件目录
-# link_directories(${CMAKE_STAGING_PREFIX}/lib/aarch64-linux-gnu)
-
-# get_directory_property(dir_link_dirs DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR} LINK_DIRECTORIES)
-# foreach(dir ${dir_link_dirs})
-#   message(STATUS "全局链接目录：${dir}")
-# endforeach()
